15c27f3a12500b9916cca0579f471bcd8936a2c7
theodom <103219157+theodom@users.noreply.github.com>
2023-03-09 22:18:49 +0100

    fixed spring calculation for extension

--- launchMechanism.py
+++ launchMechanism.py
@@ -5,7 +5,11 @@ import sympy as sym
 def find_compression(v0,theta):
     x, k = sym.symbols("x k")
     f = k*x**2 - 0.981*math.cos(theta)*x - 0.05*81
-    x_val = 0.1
-    x_sol = sym.solve(f,k)[0]
-    k_val = x_sol.subs(x,x_val)
-    return k_val 
+    k_val = 700
+    x_sol = sym.solve(f,x)[0]
+    x_val = x_sol.subs(k,k_val)
+    return x_val 
+
+def find_elongation(v0,k = 700, m = 0.050):
+    x = math.sqrt(m/(2*k))*v0
+    return x
