c13c5fb1979921af24591115ffeb800560b938f2
theodom <103219157+theodom@users.noreply.github.com>
2023-03-09 22:18:00 +0100

    fixed spring calculation for extensino

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from schuineWorp import create_bounce, create_plot
 from determineAngle import optimize
+from launchMechanism import find_compression, find_elongation
 
 #input waarden
 v0 = float(input("geef de beginsnelheid in: "))
@@ -13,3 +14,4 @@ launch_angle = optimize(v0,xt,yt)
 x,y = create_bounce(v0,launch_angle)
 x_g, y_g = create_bounce(v0,45)
 create_plot(x,y,x_guess=x_g, y_guess=y_g,x_target=xt,y_target=yt)
+print("move the spring ",float(int(find_elongation(v0,k = 1400)*1000)/10.0), "cm backwards.")
